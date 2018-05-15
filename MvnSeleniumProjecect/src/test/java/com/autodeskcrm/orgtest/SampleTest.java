f8bd4ba29b12540551e0f137d7f4e5aa0c298e91
deepak12 <sdetbatch2018@gmail.com>
2018-05-15 18:19:42 +0530

    deepak new test commit

---
+++ MvnSeleniumProjecect/src/test/java/com/autodeskcrm/orgtest/SampleTest.java
@@ -0,0 +1,12 @@
+package com.autodeskcrm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void createSample1(){
+		System.out.println("execute");
+	}
+
+}
